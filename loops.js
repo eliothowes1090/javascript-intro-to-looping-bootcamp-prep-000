a5d66cf05eaf380f65ee2df26d85a97a31f4df8e
Eliot Howes <ejhowes@gmail.com>
2019-04-27 21:29:32 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,6 +16,12 @@ function whileLoop(n) {
   return "done"
 };
 
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+
 function doWhileLoop(num) {
-  do {console.log("I run once regardless.") (num++)} while (num === 0)
+  do {console.log("I run once regardless.")} while (incrementVariable() === 0)
 };
